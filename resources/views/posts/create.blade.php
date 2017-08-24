6aead718d9e1c165bcafa26d8e69c265a6eaa19d
Chris Landon <chris@automashion.com>
2017-08-24 16:21:15 -0700

    Part 2

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create a Post</div>
+                <div class="panel-body">
+                    <form action="/posts" method="POST">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Content</label>
+                            <textarea name="content" class="form-control" rows="8" required></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success">Save</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
